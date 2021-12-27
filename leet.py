384e90939a36d1087ee033603061db3c0a1155be
silentnoodlemaster <10667809+silentnoodlemaster@users.noreply.github.com>
2021-12-27 12:00:06 +0200

    update list

--- leet.py
+++ leet.py
@@ -6,10 +6,12 @@ import os
 from datetime import *
 from dateutil.relativedelta import *
 import time
+import cloudscraper
 
 class Leet:
   def __init__(self):
     cache_dir = os.path.expanduser("~/.cache/leet")
+    self.scraper = cloudscraper.create_scraper(debug=True)
     if not os.path.exists(cache_dir):
       os.mkdir(cache_dir)
     self.cache_file = os.path.join(cache_dir, "data.json")
@@ -24,7 +26,8 @@ class Leet:
     root = "https://1337x.to"
     base = root+"/johncena141-torrents/"
     first = base+"1/"
-    r = requests.get(first)
+    # r = requests.get(first)
+    r = self.scraper.get(first)
     soup = BeautifulSoup(r.text, 'html.parser')
     last = int(soup.find("li", {"class": "last"}).a["href"].split("/")[-2])
     def get_page(soup):
@@ -46,7 +49,8 @@ class Leet:
 
             self.items.append({"name":a.text, "url": root + a["href"], "date":date.strftime("%Y-%m-%d"), "size":size})
     def get_pages(url):
-      r = requests.get(url)
+      #r = requests.get(url)
+      r = self.scraper.get(url)
       soup = BeautifulSoup(r.text, 'html.parser')
       get_page(soup)
     get_page(soup)
